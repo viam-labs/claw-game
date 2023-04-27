b689520ee72f5c151dcb2e29b62c0363ebd7ab29
Jack Damon <jackdamon@Jacks-MacBook-Air.local>
2023-04-27 13:24:34 -0400

    installed tailwind and got basic styles working

---
+++ viam-claw-typescript/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './static/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
